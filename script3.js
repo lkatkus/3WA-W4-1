8e058894483309a3d1d577bc78f9b80e265835f3
lkatkus <laimonas@katkus.eu>
2018-01-15 16:40:09 +0200

    final commit

--- script3.js
+++ script3.js
@@ -3,28 +3,32 @@ function generateTable(){
     var table = document.createElement("table");
     tableContainer.appendChild(table);
 
+    var maxNum = 5;
     var x = "";
     var y = 1;
 
-    for(let i = 0; i <= 10; i++){
+    for(let i = 0; i <= maxNum; i++){
         var newTr = document.createElement("tr");
         table.appendChild(newTr);
 
-        for(let j = 0; j <= 10; j++){
+        for(let j = 0; j <= maxNum; j++){
             if(i==0){
                 let newTd = document.createElement("td");
                 newTr.appendChild(newTd);
                 newTd.appendChild(document.createTextNode(x));
                 x++;
+
             }else if(j==0){
                 let newTd = document.createElement("td");
                 newTr.appendChild(newTd);
                 newTd.appendChild(document.createTextNode(y));
                 y++;
+
             }else{
                 let newTd = document.createElement("td");
                 newTr.appendChild(newTd);
                 newTd.appendChild(document.createTextNode(i * j));
+                newTd.daugyba = i + " x " + j; /* saves the value to that exact TD */
 
                 var parentRow;
                 var newTdIndex = 0;
@@ -33,7 +37,8 @@ function generateTable(){
 
                 newTd.addEventListener("mouseover",function(){
                     var tableCalc = document.getElementById("tableCalc");
-                    tableCalc.innerHTML = i + " x " + j;
+                    // tableCalc.innerHTML = i + " x " + j;
+                    tableCalc.textContent = this.daugyba;
 
                     parentRow = newTd.parentElement.childNodes; /*setting parent row */
                     parentTable = table.childNodes;
@@ -61,14 +66,16 @@ function generateTable(){
 
                 newTd.addEventListener("mouseleave",function(){
                     var tableCalc = document.getElementById("tableCalc");
-                    tableCalc.innerHTML = "x";
+                    tableCalc.textContent = "<h1>lalal</h1>"; /* innerHTML adds html with "styling". innerText is better this way just to add data */
 
                     newTd.classList.remove("red");
 
+                    // HORIZONTAL COLORING
                     for(var m = 0; m < parentRow.length; m++){
                         parentRow[m].classList.remove("grey");
                     }
 
+                    // VERTICAL COLORING
                     for(let m = 1; m < parentTable.length; m++){
                         prevRow = parentTable[m].childNodes;
                         prevRow[newTdIndex].classList.remove("grey");
