0d27413dafaecf238463d059312ae8d025c533a2
lkatkus <laimonas@katkus.eu>
2018-01-15 15:48:23 +0200

    Added coloring

--- script3.js
+++ script3.js
@@ -6,11 +6,11 @@ function generateTable(){
     var x = "";
     var y = 1;
 
-    for(let i = 0; i < 10; i++){
-        let newTr = document.createElement("tr");
+    for(let i = 0; i <= 10; i++){
+        var newTr = document.createElement("tr");
         table.appendChild(newTr);
 
-        for(let j = 0; j < 10; j++){
+        for(let j = 0; j <= 10; j++){
             if(i==0){
                 let newTd = document.createElement("td");
                 newTr.appendChild(newTd);
@@ -25,6 +25,57 @@ function generateTable(){
                 let newTd = document.createElement("td");
                 newTr.appendChild(newTd);
                 newTd.appendChild(document.createTextNode(i * j));
+
+                var parentRow;
+                var newTdIndex = 0;
+                var prevRow;
+                var parentTable;
+
+                newTd.addEventListener("mouseover",function(){
+                    var tableCalc = document.getElementById("tableCalc");
+                    tableCalc.innerHTML = i + " x " + j;
+
+                    parentRow = newTd.parentElement.childNodes; /*setting parent row */
+                    parentTable = table.childNodes;
+
+                    newTd.classList.add("red");
+
+                    // HORIZONTAL COLORING
+                    for(let m = 0; m < parentRow.length; m++){
+                        parentRow[m].classList.add("grey");
+                        if(parentRow[m] === newTd){
+                            newTdIndex = m;
+                            // break; /* comment out to span coloring to the end */
+                        }
+                    }
+
+                    // VERTICAL COLORING
+                    for(let m = 1; m < parentTable.length; m++){
+                        prevRow = parentTable[m].childNodes;
+                        prevRow[newTdIndex].classList.add("grey");
+                        // if(prevRow[newTdIndex] === newTd){ /* comment out to stop coloring to the end */
+                        //     break;
+                        // }
+                    }
+                })
+
+                newTd.addEventListener("mouseleave",function(){
+                    var tableCalc = document.getElementById("tableCalc");
+                    tableCalc.innerHTML = "x";
+
+                    newTd.classList.remove("red");
+
+                    for(var m = 0; m < parentRow.length; m++){
+                        parentRow[m].classList.remove("grey");
+                    }
+
+                    for(let m = 1; m < parentTable.length; m++){
+                        prevRow = parentTable[m].childNodes;
+                        prevRow[newTdIndex].classList.remove("grey");
+                    }
+
+                })
+
             }
         }
     }
